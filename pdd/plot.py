c03d7f9fdda26ffe67eb0c1e2204058082ffee1d
Artem <webstermaster777@gmail.com>
2020-02-04 18:03:37 +0300

    reworked plot

--- pdd/plot.py
+++ pdd/plot.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot(embeddings,labels,classes_name,name,colors):
-  classes_count=len(classes_name)
+def plot(embeddings,labels,class_name,name,colors):
   plt.figure(figsize=(10,10))
-  
-  for i in range(classes_count):
+  for i in range(len(class_name)):
       inds = np.where(labels==i)[0]
       plt.scatter(embeddings[inds,0],embeddings[inds,1],color=colors[i])
-  plt.legend(classes_name,loc='center left', bbox_to_anchor=(1, 0.5))
+  plt.legend(class_name,loc='center left', bbox_to_anchor=(1, 0.5))
   plt.title(name)
   plt.show()
