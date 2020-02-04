fbd447739430d25211bd1534c95942a206909f01
Artem <webstermaster777@gmail.com>
2020-02-04 18:32:54 +0300

    Plot changes

--- pdd/plot.py
+++ pdd/plot.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot(embeddings,labels,class_name,name,colors):
-  plt.figure(figsize=(10,10))
+  fig=plt.figure(figsize=(10,10))
   for i in range(len(class_name)):
       inds = np.where(labels==i)[0]
       plt.scatter(embeddings[inds,0],embeddings[inds,1],color=colors[i])
   plt.legend(class_name,loc='center left', bbox_to_anchor=(1, 0.5))
   plt.title(name)
   plt.show()
-  plt.figure.canvas.draw()
-  plt.figure.savefig( f'./PDD.png' )
+  fig.canvas.draw()
+  fig.savefig(f'./PDD.png')
