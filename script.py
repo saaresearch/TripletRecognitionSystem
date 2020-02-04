c826f109730bbfcd3395a7cc1ff9c976c7b69f84
Артем Сметанин <webstermaster777@gmail.com>
2020-02-04 21:10:47 +0300

    reworked script

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 from pdd.model import PDDModel
+import torch
 
-def get_predict()
+def get_predict():
+    model=PDDModel(1280,15,True)
+    model.load_state_dict(torch.load(PATH))
+    model.eval()
+
+get_predict()
