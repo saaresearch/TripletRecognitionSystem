282ca84f056a536fa0141219bca1e7569f27cf0b
Artem <webstermaster777@gmail.com>
2020-02-21 19:49:48 +0300

    add enter to json and parse json

--- script.py
+++ script.py
@@ -10,8 +10,9 @@ from scipy.spatial.distance import cosine
 import pickle
 from torchvision import transforms
 from torchvision import transforms, datasets
-import argparse
+
 import yaml
+import json
 
 
 def load_image(infilename):
@@ -39,6 +40,58 @@ def load_config(config_file):
         return yaml.load(f, Loader=yaml.FullLoader)
 
 
+def show_predict(filename):
+    with open(filename) as f:
+        items = json.load(f)
+    print('\033[1m' + "Network prediction:")
+    item_prediction = items['prediction']
+    print('\033[0m' + "Class label:", item_prediction['label'])
+    print("Class name:", item_prediction['class_name'])
+    item_knn = items['knn parameters']
+    print('\033[1m' + "KNN parameters:")
+    print('\033[0m' + "Count neighbors:", item_knn['n_neighbors'])
+    print('\033[0m' + "Metrics:", item_knn['metrics'])
+    item_topn = items['topn']
+    print('\033[1m' + "TOP 5 predictions :")
+    for predict in item_topn:
+        print('\033[0m' + "label:", str(predict['label']),
+             "Image index:", str(predict['index']),
+             "Distance:", str(predict['distance']), sep="  ")
+
+
+def create_json_file(label, class_name, knn_model, distances, indices):
+
+    data = {
+        "prediction": {
+            "label": str(label),
+            "class_name": str(class_name[label])
+
+
+        },
+        "knn parameters":
+        {
+            "n_neighbors": str(knn_model.n_neighbors),
+            "metrics": str(knn_model.metric.__name__)
+        },
+        "topn": [
+            {"label": str(knn_model._y[indices[0][0]]), "index":str(
+                indices[0][0]), "distance": "%.4f" % (distances[0][0])},
+            {"label": str(knn_model._y[indices[0][1]]), "index":str(
+                indices[0][1]), "distance": "%.4f" % (distances[0][1])},
+            {"label": str(knn_model._y[indices[0][2]]), "index":str(
+                indices[0][2]), "distance": "%.4f" % (distances[0][2])},
+            {"label": str(knn_model._y[indices[0][3]]), "index":str(
+                indices[0][3]), "distance": "%.4f" % (distances[0][3])},
+            {"label": str(knn_model._y[indices[0][4]]), "index":str(
+                indices[0][4]), "distance": "%.4f" % (distances[0][4])}
+
+        ]
+
+    }
+    with open("data_file.json", "w") as write_file:
+        json.dump(data, write_file, indent=2)
+
+
 def get_predict(img_name, triplet_model_weight, knn_model_weight, class_name):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = PDDModel(1280, 15, True)
@@ -54,12 +107,9 @@ def get_predict(img_name, triplet_model_weight, knn_model_weight, class_name):
 
     classes_name = load_text_file(class_name)
     y_pred = knn.predict(embedding)
-    # distances, indices = knn.kneighbors(embedding[[0]], n_neighbors=3)
-    # print(distances, indices)
-    # print(indices.ravel().__dir__())
-    # print(indices.data)
-    # print(y_pred)
-    print(classes_name[y_pred[0]])
+    distances, indices = knn.kneighbors(embedding[[0]], n_neighbors=10)
+    create_json_file(y_pred[0], classes_name, knn, distances, indices)
+    show_predict('data_file.json')
 
 
 def main():
