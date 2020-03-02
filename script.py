b6dbfa8d1cbeffb9d0739eb3721efb14c64b095e
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2020-03-02 14:06:56 +0300

    fix some pull request issues

--- script.py
+++ script.py
@@ -1,19 +1,17 @@
-from pdd.model import PDDModel
-import torch
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from PIL import Image
 import numpy as np
-from skimage.transform import resize
-import torch
-from scipy.spatial.distance import cosine
 import pickle
-from torchvision import (
-    transforms, 
+import torch
+import json
+
+from sklearn.neighbors import KNeighborsClassifier
+from torchvision import transforms
+from PIL import Image
+
+from pdd.model import PDDModel
+from pdd.data_utils import (
+    load_config,
+    write_json_file
 )
-from pdd.data_utils import load_config
-from pdd.data_utils import write_json_file
-from json import load
 
 
 def load_image(infilename):
@@ -27,67 +25,60 @@ def load_image(infilename):
     return img
 
 
-def load_text_file(filename):
-    classes_name = []
-    with open(filename, 'r') as filehandle:
-        for line in filehandle:
-            currentPlace = line[:-1]
-            classes_name.append(currentPlace)
-    return classes_name
-
-def show_predict(filename):
-    with open(filename) as f:
-        items = load(f)
-    print('\033[1m' + "Network prediction:")
-    item_prediction = items['prediction']
-    print('\033[0m' + "Class label:", item_prediction['label'])
-    print("Class name:", item_prediction['class_name'])
-    item_knn = items['knn parameters']
-    print('\033[1m' + "KNN parameters:")
-    print('\033[0m' + "Count neighbors:", item_knn['n_neighbors'])
-    print('\033[0m' + "Metrics:", item_knn['metrics'])
-    item_topn = items['topn']
-    print('\033[1m' + "TOP 5 predictions :")
-    for predict in item_topn:
-        print('\033[0m' + "label:", str(predict['label']),
-              "Image index:", str(predict['index']),
-              "Distance:", str(predict['distance']), sep="  ")
+def load_class_names(filename):
+  with open(filename) as f:
+    return f.read().splitlines()
 
 
-def create_json_file(label, class_name, knn_model, distances, indices,savefile_name):
+def show_predict(pred):
+    print("Network prediction:")
+    item_prediction = pred['prediction']
+    print(
+        f"\tClass label: {item_prediction['label']}\n"
+        f"\tClass name: {item_prediction['class_name']}\n"
+    )
 
-    data = {
+    item_knn = pred['knn_parameters']
+    print("KNN parameters:")
+    print(
+        f"\tCount neighbors: {item_knn['n_neighbors']}\n"
+        f"\tMetric: {item_knn['metrics']}\n"
+    )
+
+    print(f"TOP {len(pred['topn'])} predictions:")
+    for predict in pred['topn']:
+        print(f"\tLabel: {predict['label']} "
+              f"Image index: {predict['index']}",
+              f"Distance: {predict['distance']:.4f}"
+        )
+
+
+def create_response(label, class_names, knn_model, indices, distances):
+    response = {
         "prediction": {
-            "label": str(label),
-            "class_name": str(class_name[label])
+            "label": int(label),
+            "class_name": str(class_names[label])
         },
-        "knn parameters":
-        {
+        "knn_parameters": {
             "n_neighbors": knn_model.n_neighbors,
             "metrics": knn_model.metric.__name__
         },
-        "topn": [
-            {"label": str(knn_model._y[indices[0][0]]), "index":str(
-                indices[0][0]), "distance": "%.4f" % (distances[0][0])},
-            {"label": str(knn_model._y[indices[0][1]]), "index":str(
-                indices[0][1]), "distance": "%.4f" % (distances[0][1])},
-            {"label": str(knn_model._y[indices[0][2]]), "index":str(
-                indices[0][2]), "distance": "%.4f" % (distances[0][2])},
-            {"label": str(knn_model._y[indices[0][3]]), "index":str(
-                indices[0][3]), "distance": "%.4f" % (distances[0][3])},
-            {"label": str(knn_model._y[indices[0][4]]), "index":str(
-                indices[0][4]), "distance": "%.4f" % (distances[0][4])}
-        ]
-
+        "topn": []
     }
-    write_json_file(savefile_name,data,2)
-   
+
+    for idx, dist in zip(indices, distances):
+        response["topn"].append({
+            "label": int(knn_model._y[idx]),
+            "index": int(idx),
+            "distance": float(dist)
+        })
+
+    return response   
 
 
-def get_predict(img_name, feature_extractor, classifier, class_names,prediction_savefile):
+def get_predict(img_name, feature_extractor, classifier, class_names, topn):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = PDDModel(1280, 15, True)
-    knn = KNeighborsClassifier(3, metric=cosine)
     model.load_state_dict(
         torch.load(
             feature_extractor,
@@ -97,24 +88,25 @@ def get_predict(img_name, feature_extractor, classifier, class_names,prediction_
     embedding = model(inputs).detach().cpu().numpy()
     knn = pickle.load(open(classifier, 'rb'))
 
-    classes_name = load_text_file(class_names)
+    classes_name = load_class_names(class_names)
     y_pred = knn.predict(embedding)
-    distances, indices = knn.kneighbors(embedding[[0]], n_neighbors=10)
-    create_json_file(y_pred[0], classes_name, knn, distances, indices,prediction_savefile)
+    distances, indices = knn.kneighbors(embedding[[0]], n_neighbors=topn)
+    return create_response(y_pred[0], classes_name, knn, indices[0], distances[0])
     
 
-
 def main():
-    # path=input()
-    # get_predict(path)
     config = load_config('config/script_parametrs.yaml')
-    get_predict(
+    pred = get_predict(
         config['img_path'],
         config['feature_extractor'],
         config['classifier'],
         config['class_names'],
-        config['prediction_savefile'])
-    show_predict('data_file.json')
+        config['topn']
+    )
+    show_predict(pred)
+    if config['prediction_savefile']:
+        write_json_file(config['prediction_savefile'], pred)
+
 
 if __name__ == '__main__':
     main()
