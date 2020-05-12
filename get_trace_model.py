f64a09a441526c5bc7c82fd5201a1dc0cb629f0e
Artem <webstermaster777@gmail.com>
2020-05-12 21:34:18 +0300

    update

--- get_trace_model.py
+++ get_trace_model.py
@@ -1,6 +1,14 @@
-import os 
+import os
 
-os.system('python train.py')
-os.system('rm -r data')
-os.system('python classifier_train.py')
-os.system('python trace.py')
+
+def main():
+    os.system('python train.py')
+    os.system('rm -r data')
+    os.system('python classifier_train.py')
+    os.system('python trace.py')
+    os.system('find . ! -name "trace_pddmodel.pt""classes2.txt" -delete')
+
+
+if __name__ == "__main__":
+
+    main()
