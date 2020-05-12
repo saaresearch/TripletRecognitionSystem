78c9d52f153e695e3ff336cdbfbf8e7fab5bffa4
Artem <webstermaster777@gmail.com>
2020-05-12 21:46:16 +0300

    update

--- get_trace_model.py
+++ get_trace_model.py
@@ -6,7 +6,7 @@ def main():
     os.system('rm -r data')
     os.system('python classifier_train.py')
     os.system('python trace.py')
-    os.system('find . ! -name "trace_pddmodel.pt""classes2.txt" -delete')
+    os.system('find . ! -name "trace_pddmodel.pt" -name "classes2.txt" -delete')
 
 
 if __name__ == "__main__":
