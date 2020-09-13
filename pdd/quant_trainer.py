954cf2a8f03c50cc8ff6ee70f4f9f994e2995e00
Artem <webstermaster777@gmail.com>
2020-09-13 23:23:58 +0300

    add static quant

---
+++ pdd/quant_trainer.py
@@ -0,0 +1,165 @@
+import torch
+import numpy as np
+from tqdm import tqdm
+
+from pdd.metrics import knn_acc
+from pdd.plot import plot
+from pdd.model import save_model
+
+COUNT_NEIGHBOR_EXP_1 = 1
+COUNT_NEIGHBOR_EXP_2 = 3
+
+
+def forward_inputs_into_model(loader, model, device, batch_size):
+    X = []
+    y = []
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs).detach().cpu().numpy()
+            targets = targets.detach().cpu().numpy()
+            if (outputs.shape[0] == batch_size):
+                X.append(outputs)
+                y.append(targets)
+    return np.vstack(X), np.hstack(y)
+
+
+class TripletTrainer(object):
+    def __init__(self,
+                 model,
+                 optimizer,
+                 train_triplet_loader,
+                 epochs,
+                 test_triplet_loader,
+                 batch_size,
+                 knn_train_loader,
+                 knn_test_loader,
+                 scheduler,
+                 plot_classes_name,
+                 num_classes,
+                 miner,
+                 loss_history,
+                 safe_plot_img_path,
+                 model_save_path,
+                 optim_save_path,
+                 knn_metric
+                 ):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = model.to(self.device)
+        self.optimizer = optimizer
+        # self.loss = loss
+        self.epochs = epochs
+        self.train_triplet_loader = train_triplet_loader
+        self.test_triplet_loader = test_triplet_loader
+        self.batch_size = batch_size
+        self.knn_train_loader = knn_train_loader
+        self.knn_test_loader = knn_test_loader
+#         self.scheduler=scheduler
+        self.plot_classes_name = plot_classes_name
+        self.num_classes = num_classes
+        self.miner = miner
+        self.loss_history = loss_history
+        self.safe_plot_img_path = safe_plot_img_path
+        self.model_save_path = model_save_path
+        self.optim_save_path = optim_save_path
+        self.knn_metric = knn_metric
+
+    def train(self):
+        for e in tqdm(range(self.epochs), desc='Epoch'):
+
+            test_em, test_labels = forward_inputs_into_model(self.knn_test_loader,
+                                                             self.model,
+                                                             self.device,
+                                                             self.batch_size)
+            train_em, train_labels = forward_inputs_into_model(self.knn_train_loader,
+                                                               self.model,
+                                                               self.device,
+                                                               self.batch_size)
+            knn_acc(
+                test_em,
+                test_labels,
+                train_em,
+                train_labels,
+                COUNT_NEIGHBOR_EXP_1,
+                self.knn_metric),
+            knn_acc(
+                test_em,
+                test_labels,
+                train_em,
+                train_labels,
+                COUNT_NEIGHBOR_EXP_2,
+                self.knn_metric)
+            plot(
+                train_em,
+                train_labels,
+                self.plot_classes_name,
+                'train_embeddings',
+                self.safe_plot_img_path)
+            plot(
+                test_em,
+                test_labels,
+                self.plot_classes_name,
+                'test_embeddings',
+                self.safe_plot_img_path)
+            self.train_phase()
+            self.validating_phase()
+            if e % 5 == 0 and e > 0:
+                save_model(
+                    self.model,
+                    self.optimizer,
+                    self.model_save_path,
+                    self.optim_save_path)
+
+    def train_phase(self):
+
+        train_n = len(self.train_triplet_loader)
+        train_loss = 0.
+        train_frac_pos = 0.
+
+        self.model.train()
+        with tqdm(self.train_triplet_loader, desc='Batch') as b_pbar:
+            for b, batch in enumerate(b_pbar):
+                self.optimizer.zero_grad()
+
+                labels, data = batch
+                labels = torch.cat([label for label in labels], axis=0)
+                data = torch.cat([datum for datum in data], axis=0)
+                labels = labels
+                data = data
+
+                embeddings = self.model(data)
+                loss, frac_pos = self.miner(labels, embeddings)
+                loss.backward()
+                self.optimizer.step()
+#                     scheduler.step()
+                train_loss += loss.detach().item()
+                train_frac_pos += frac_pos.detach().item() if frac_pos is not None else \
+                    0.
+
+                b_pbar.set_postfix(
+                    train_loss=train_loss / train_n,
+                    train_frac_pos=f'{(train_frac_pos/train_n):.2%}'
+                )
+
+    def validating_phase(self):
+        val_n = len(self.test_triplet_loader)
+        val_loss = 0.
+        val_frac_pos = 0.
+        self.model.eval()
+        with tqdm(self.test_triplet_loader, desc='val') as b_pbar:
+            for b, batch in enumerate(b_pbar):
+                labels, data = batch
+                labels = torch.cat([label for label in labels], axis=0)
+                data = torch.cat([datum for datum in data], axis=0)
+                labels = labels
+                data = data
+                embeddings = self.model(data)
+                loss, frac_pos = self.miner(labels, embeddings)
+                val_loss += loss.detach().item()
+                self.loss_history.append(val_loss)
+                val_frac_pos += frac_pos.detach().item() if frac_pos is not None else \
+                    0.
+                b_pbar.set_postfix(
+                    val_loss=val_loss / val_n,
+                    val_frac_pos=f'{( val_frac_pos / val_n ):.2%}'
+                )
---
+++ quant_train.py
@@ -0,0 +1,123 @@
+
+import numpy as np
+import torch
+import os
+import random
+
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from triplettorch import AllTripletMiner
+from triplettorch import TripletDataset
+
+from pdd.train_test_split import datadir_train_test_split
+from pdd.data_utils import AllCropsDataset
+from pdd.quant_model import PDDModel
+from pdd.trainer import TripletTrainer
+from pdd.data_utils import unzip_data
+from pdd.data_utils import load_config
+from pdd.data_utils import get_classname_file
+from train import fix_random_seed
+from train import prepare_datasets
+from train import split_on_train_and_test
+
+DEVICE = 'cpu'
+
+
+def main():
+
+    config = load_config('config/train_parameters.yaml')
+
+    fix_random_seed(config['random_seed'], config['cudnn_deterministic'])
+
+    print("Extract data")
+    unzip_data(config['data_zip_path'], config['data_save_path'])
+
+    print("Split on train and test")
+    split_on_train_and_test(
+        config['random_seed'],
+        config['data_save_path'],
+        config['test_size'])
+
+    print("Create datasets")
+    train_ds, test_ds = prepare_datasets(config['data_save_path'])
+    print("Create data loaders")
+    def train_set_d(index): return train_ds[index][0].float().numpy()
+    def test_set_d(index): return test_ds[index][0].float().numpy()
+    tri_train_set = TripletDataset(
+        torch.FloatTensor(
+            train_ds.targets).numpy(),
+        train_set_d,
+        len(train_ds),
+        config['n_sample'])
+    tri_test_set = TripletDataset(
+        torch.FloatTensor(
+            test_ds.targets),
+        test_set_d,
+        len(test_ds),
+        1)
+    tri_train_load = DataLoader(tri_train_set,
+                                batch_size=config['batch_size'],
+                                shuffle=True,
+                                num_workers=2,
+                                pin_memory=True
+                                )
+    tri_test_load = DataLoader(tri_test_set,
+                               batch_size=config['batch_size'],
+                               shuffle=False,
+                               num_workers=2,
+                               pin_memory=True
+                               )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_ds,
+        pin_memory=True,
+        batch_size=config['batch_size'],
+        shuffle=True,
+        num_workers=2)
+    test_loader = torch.utils.data.DataLoader(
+        test_ds,
+        pin_memory=True,
+        batch_size=config['batch_size'],
+        shuffle=True,
+        num_workers=2)
+    print("Create miner")
+    miner = AllTripletMiner(.5)
+
+    print("Build computational graph")
+    model = PDDModel(1280, len(train_ds.classes), True)
+    model = model.to(DEVICE)
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=config['lr'],
+        weight_decay=config['weight_decay'])
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=config['step_size'], gamma=config['gamma'])
+    print("Train model")
+    fix_random_seed(config['random_seed'], config['cudnn_deterministic'])
+    loss_history = []
+    trainer = TripletTrainer(
+        model=model,
+        optimizer=optimizer,
+        train_triplet_loader=tri_train_load,
+        epochs=config['epochs'],
+        test_triplet_loader=tri_test_load,
+        batch_size=config['batch_size'],
+        knn_train_loader=train_loader,
+        knn_test_loader=test_loader,
+        scheduler=scheduler,
+        plot_classes_name=test_ds.classes,
+        num_classes=config['num_classes'],
+        miner=miner,
+        loss_history=loss_history,
+        safe_plot_img_path=config['plot_embeddings_img'],
+        model_save_path=config['model_save_path'],
+        optim_save_path=config['optim_save_path'],
+        knn_metric=config['knn_metric']
+    )
+    get_classname_file(train_ds.classes)
+    trainer.train()
+
+
+if __name__ == "__main__":
+
+    main()
